20da7ad4544568349b607ab1ce1f4e1998353539
Rüdiger Heins <ruediger.heins@iteratec.de>
2018-04-19 20:04:08 +0200

    implements support for PROCESS targets

--- src/nikto_worker.rb
+++ src/nikto_worker.rb
@@ -8,10 +8,17 @@ require_relative "./nikto_configuration"
 class NiktoWorker < CamundaWorker
   def work(task)
 
+    # Quite Ugly isn't it?
+    process_targets = task.dig('variables', 'PROCESS_TARGETS', 'value')
+    process_targets[0]=""
+    process_targets[process_targets.length-1]=""
+    process_targets.gsub!('\\','')
+    process_targets = JSON.parse(process_targets)
+
     config = NiktoConfiguration.new
-    config.nikto_target = task.dig('variables', 'NIKTO_TARGET', 'value')
-    config.nikto_ports = task.dig('variables', 'NIKTO_PORTS', 'value')
-    config.nikto_parameter = task.dig('variables', 'NIKTO_PARAMETER', 'value')
+    config.nikto_target = process_targets[0].dig('location')
+    config.nikto_ports = process_targets[0].dig('attributes', 'NIKTO_PORTS')
+    config.nikto_parameter = process_targets[0].dig('attributes', 'NIKTO_PARAMETER')
 
     scan = NiktoScan.new(task.dig('id'), config)
     scan.start
